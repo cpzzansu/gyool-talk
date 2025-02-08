a0f85a83806322042bb8b61050ef4a5647f6481d
OHDONGRIM <odong0816@gmail.com>
2025-02-08 21:29:08 +0900

    ap 연동 테스트

--- app/Login.tsx
+++ app/Login.tsx
@@ -6,105 +6,139 @@ import {
   View,
   Text,
   Alert,
+  Platform,
 } from "react-native";
-import { Stack, useRouter } from "expo-router";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 export default function LoginScreen() {
-  const router = useRouter(); // 페이지 이동을 위한 useRouter 사용
   const [userId, setUserId] = useState(""); // 아이디 상태 관리
   const [password, setPassword] = useState(""); // 비밀번호 상태 관리
+  const navigation = useNavigation(); // navigation 객체
+
+  const showAlert = (title: string, message: string) => {
+    if (Platform.OS === "web") {
+      window.alert(`${title}\n\n${message}`);
+    } else {
+      Alert.alert(title, message);
+    }
+  };
 
   const login = () => {
-    console.log("아이디 로그인");
+    if (userId.trim() === "") {
+      showAlert("아이디 입력 확인", "아이디가 입력되지 않았습니다.");
+    } else if (password.trim() === "") {
+      showAlert("비밀번호 입력 확인", "비밀번호가 입력되지 않았습니다.");
+    } else {
+      axios
+        .post("http://localhost:8080/login", { id: userId, pwd: password })
+        .then(function (resp: any) {
+          console.log(resp.data);
+          if (resp.data !== null && resp.data != "") {
+            console.log("로그인 성공");
+          } else {
+            showAlert("로그인 실패", "아이디나 비밀번호를 확인하세요.");
+            setUserId("");
+            setPassword("");
+          }
+        })
+        .catch(function (err: any) {
+          console.log(`Error Message: ${err}`);
+        });
+    }
   };
-  const snsLogin = (sns) => {
+
+  const snsLogin = (sns: string) => {
     console.log(`${sns} 로그인`);
   };
+
   const findId = () => {
-    console.log("아이디 찾기");
+    navigation.navigate("FindId");
   };
+
   const resetPw = () => {
-    console.log("비밀번호 재설정");
+    navigation.navigate("ResetPw");
   };
+
   const join = () => {
-    console.log("회원가입");
+    navigation.navigate("Join");
   };
 
   return (
-    <>
-      <ThemedView style={styles.container}>
-        {/* 로고 */}
-        <Image
-          source={require("@/assets/images/gyoolTalk.png")}
-          style={styles.logo}
-        />
-        <ThemedText type="title" style={{ fontSize: 24, marginBottom: 40 }}>
-          귤톡
-        </ThemedText>
+    <ThemedView style={styles.container}>
+      {/* 로고 */}
+      <Image
+        source={require("@/assets/images/gyoolTalk.png")}
+        style={styles.logo}
+      />
+      <ThemedText type="title" style={{ fontSize: 24, marginBottom: 40 }}>
+        귤톡
+      </ThemedText>
 
-        {/* 아이디 입력창 */}
-        <TextInput
-          style={styles.input}
-          placeholder="아이디"
-          value={userId}
-          onChangeText={setUserId}
-          placeholderTextColor="#827F7F"
-        />
+      {/* 아이디 입력창 */}
+      <TextInput
+        style={styles.input}
+        placeholder="아이디"
+        value={userId}
+        onChangeText={setUserId}
+        placeholderTextColor="#827F7F"
+      />
 
-        {/* 비밀번호 입력창 */}
-        <TextInput
-          style={styles.input}
-          placeholder="비밀번호"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-          placeholderTextColor="#827F7F"
-        />
+      {/* 비밀번호 입력창 */}
+      <TextInput
+        style={styles.input}
+        placeholder="비밀번호"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        placeholderTextColor="#827F7F"
+      />
 
-        {/* 로그인 버튼 */}
-        <TouchableOpacity style={styles.loginButton} onPress={login}>
-          <ThemedText style={styles.loginButtonText}>로그인</ThemedText>
+      {/* 로그인 버튼 */}
+      <TouchableOpacity style={styles.loginButton} onPress={login}>
+        <ThemedText style={styles.loginButtonText}>로그인</ThemedText>
+      </TouchableOpacity>
+
+      <View style={styles.orContainer}>
+        <View style={styles.line} />
+        <Text style={styles.orText}>OR</Text>
+        <View style={styles.line} />
+      </View>
+
+      {/* SNS 로그인 버튼들 */}
+      <View style={styles.rowContainer}>
+        <TouchableOpacity onPress={() => snsLogin("naver")}>
+          <Image
+            source={require("@/assets/images/naver_login.png")}
+            style={styles.loginImage}
+          />
         </TouchableOpacity>
+        <TouchableOpacity onPress={() => snsLogin("google")}>
+          <Image
+            source={require("@/assets/images/google_login.png")}
+            style={styles.loginImage}
+          />
+        </TouchableOpacity>
+      </View>
 
-        <View style={styles.orContainer}>
-          <View style={styles.line} />
-          <Text style={styles.orText}>OR</Text>
-          <View style={styles.line} />
-        </View>
-
-        <View style={styles.rowContainer}>
-          <TouchableOpacity onPress={() => snsLogin("naver")}>
-            <Image
-              source={require("@/assets/images/naver_login.png")} // 이미지 경로
-              style={styles.loginImage}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => snsLogin("google")}>
-            <Image
-              source={require("@/assets/images/google_login.png")} // 이미지 경로
-              style={styles.loginImage}
-            />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.textRow}>
-          <TouchableOpacity onPress={() => findId}>
-            <Text style={styles.textButton}>아이디 찾기</Text>
-          </TouchableOpacity>
-          <Text style={styles.textButton}> | </Text>
-          <TouchableOpacity onPress={() => resetPw}>
-            <Text style={styles.textButton}>비밀번호 재설정</Text>
-          </TouchableOpacity>
-          <Text style={styles.textButton}> | </Text>
-          <TouchableOpacity onPress={() => join}>
-            <Text style={styles.textButton}>회원가입</Text>
-          </TouchableOpacity>
-        </View>
-      </ThemedView>
-    </>
+      {/* 아이디 찾기, 비밀번호 재설정, 회원가입 링크 */}
+      <View style={styles.textRow}>
+        <TouchableOpacity onPress={findId}>
+          <Text style={styles.textButton}>아이디 찾기</Text>
+        </TouchableOpacity>
+        <Text style={styles.textButton}> | </Text>
+        <TouchableOpacity onPress={resetPw}>
+          <Text style={styles.textButton}>비밀번호 재설정</Text>
+        </TouchableOpacity>
+        <Text style={styles.textButton}> | </Text>
+        <TouchableOpacity onPress={join}>
+          <Text style={styles.textButton}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
+    </ThemedView>
   );
 }
 
@@ -112,25 +146,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    //justifyContent: 'center',
     padding: 20,
     backgroundColor: "#DCD7CB",
-  },
-  headerStyle: {
-    backgroundColor: "#DCD7CB",
-  },
-  headerTitleStyle: {
-    fontWeight: "bold",
-    fontSize: 24,
   },
   logo: {
     marginTop: 110,
     width: 44,
     height: 48,
-    marginBottom: 10,
-  },
-  title: {
-    fontSize: 24,
     marginBottom: 10,
   },
   input: {
@@ -171,28 +193,28 @@ const styles = StyleSheet.create({
     color: "#414040",
   },
   loginImage: {
-    width: 190, // 원하는 크기
+    width: 190,
     height: 41,
     borderRadius: 6,
     borderColor: "#858886",
     borderWidth: 0.5,
   },
   rowContainer: {
-    flexDirection: "row", // 가로 정렬
-    justifyContent: "space-around", // 요소 간격 조정 (center, space-around 등 가능)
-    alignItems: "center", // 수직 정렬
-    marginTop: 5, // 위쪽 여백
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 5,
     gap: 5,
   },
   textRow: {
-    flexDirection: "row", // 가로 정렬
-    justifyContent: "center", // 가운데 정렬
-    alignItems: "center", // 수직 정렬
-    marginTop: 20, // 위쪽 여백
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
   },
   textButton: {
     fontSize: 14,
     color: "#414040",
-    marginHorizontal: 3, // 좌우 간격 추가
+    marginHorizontal: 3,
   },
 });
