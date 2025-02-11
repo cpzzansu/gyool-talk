3ce4bc14f5f2d569e3d5e0db48f7e00a521c0622
공룡현정 <138553318+hyeonjeongeee@users.noreply.github.com>
2025-02-11 21:04:15 +0900

    반응형 적용 했지만 조금 구린

--- app/Join.tsx
+++ app/Join.tsx
@@ -21,7 +21,8 @@ export default function LoginScreen() {
   const [nickname, setNickName] = useState(""); // 닉네임 상태 관리
 
   const [isDuplicate, setIsDuplicate] = useState<boolean | null>(null); // 아이디 중복 여부
-  const [isConfirm, setIsIsConfirm] = useState<boolean | null>(null); // 아이디 중복 여부
+  const [isConfirm, setIsConfirm] = useState<boolean | null>(null); // 아이디 중복 여부
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null); // 아이디 중복 여부
 
   const idCheck = () => {
     console.log("아이디 중복체크");
@@ -35,10 +36,16 @@ export default function LoginScreen() {
 
   const emailConfirm = () => {
     console.log("이메일 인증 체크");
-    setIsIsConfirm(true);
+    setIsConfirm(true);
   };
   const cofirmNumCheck = () => {
     console.log("인증번호 확인 체크");
+    const randomValue = Math.random() < 0.5 ? 0 : 1;
+    if (randomValue == 0) {
+      setIsCorrect(true);
+    } else {
+      setIsCorrect(false);
+    }
   };
   const join = () => {
     console.log(width);
@@ -49,7 +56,7 @@ export default function LoginScreen() {
     container: {
       flex: 1,
       alignItems: "center",
-      padding: 20,
+      padding: width * 0.045,
       backgroundColor: "#DCD7CB",
     },
     defaultExplain: {
@@ -61,12 +68,23 @@ export default function LoginScreen() {
       marginBottom: height * 0.07,
       fontWeight: "500",
     },
+    largeMb: {
+      marginBottom: height * 0.017,
+    },
+    smallMb: {
+      marginBottom: height * 0.006,
+    },
+    inputGrp: {
+      paddingHorizontal: width * 0.01,
+      backgroundColor: "#DCD7CB",
+      width: "100%",
+    },
     inputBlock: {
-      width: width * 0.725,
       height: height * 0.048,
       borderRadius: 3,
-      paddingHorizontal: 10,
+      paddingHorizontal: width * 0.027,
       backgroundColor: "#EFEFEF",
+      flex: 1,
     },
     buttonBlock: {
       width: width * 0.15,
@@ -77,76 +95,51 @@ export default function LoginScreen() {
       borderRadius: 3,
     },
     input: {
-      width: width * 0.91,
+      width: "100%",
       height: height * 0.048,
       borderRadius: 3,
-      paddingHorizontal: 10,
+      paddingHorizontal: width * 0.027,
       backgroundColor: "#EFEFEF",
-    },
-    group: {
-      marginTop: 13,
-      width: "100%",
     },
     twoBlock: {
       display: "flex",
       flexDirection: "row",
-    },
-    logo: {
-      marginTop: 110,
-      width: 44,
-      height: 48,
-      marginBottom: 10,
+      width: "100%",
     },
     title: {
-      fontSize: 24,
-      marginBottom: 10,
-    },
-    oneBlock: {
-      width: "82.8%",
-      height: 46,
-      borderRadius: 3,
-      paddingHorizontal: 10,
-      backgroundColor: "#EFEFEF",
-    },
-    checkBtn: {
-      width: "17.2%",
-      height: 45,
-      marginLeft: 14,
-      backgroundColor: "#EF7417",
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: 3,
+      fontSize: width * 0.054,
+      marginBottom: width * 0.027,
     },
     checkText: {
       color: "#F1F1F1",
-      fontSize: 20,
+      fontSize: width * 0.045,
       fontWeight: "bold",
     },
     joinButton: {
       width: "100%",
-      height: 45,
+      height: height * 0.048,
       backgroundColor: "#EF7417",
       alignItems: "center",
       justifyContent: "center",
       borderRadius: 3,
-      marginTop: 50,
+      marginTop: height * 0.079,
     },
     joinButtonText: {
       color: "#F1F1F1",
-      fontSize: 20,
+      fontSize: width * 0.045,
       fontWeight: "bold",
     },
-    chkMsg: {
-      width: "97%",
-      lineHeight: 13,
-      fontSize: 13,
-      marginTop: 5,
-    },
     okId: {
+      fontSize: width * 0.029,
       color: "#299f0f",
     },
     duplId: {
+      fontSize: width * 0.029,
       color: "#d61212",
+    },
+    isText: {
+      display: "flex",
+      flexDirection: "column",
     },
   });
   return (
@@ -158,87 +151,102 @@ export default function LoginScreen() {
             기본 정보를 입력하세요.
           </ThemedText>
         </View>
-
-        {/* 아이디 입력창 */}
-        <View style={styles.twoBlock}>
+        <ThemedView style={styles.inputGrp}>
+          {/* 아이디 입력창 */}
+          <View style={[styles.isText, styles.largeMb]}>
+            <View style={[styles.twoBlock]}>
+              <TextInput
+                style={styles.inputBlock}
+                placeholder="아이디"
+                value={username}
+                onChangeText={setUsername}
+                placeholderTextColor="#827F7F"
+              />
+              <TouchableOpacity style={styles.buttonBlock} onPress={idCheck}>
+                <ThemedText style={styles.checkText}>확인</ThemedText>
+              </TouchableOpacity>
+            </View>
+            {isDuplicate !== null && (
+              <ThemedText style={isDuplicate ? styles.duplId : styles.okId}>
+                {isDuplicate
+                  ? "사용 중인 아이디입니다."
+                  : "사용 가능한 아이디입니다."}
+              </ThemedText>
+            )}
+          </View>
+          {/* 비밀번호 입력창 */}
           <TextInput
-            style={styles.inputBlock}
-            placeholder="아이디"
-            value={username}
-            onChangeText={setUsername}
+            style={[styles.input, styles.smallMb]}
+            placeholder="비밀번호"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
             placeholderTextColor="#827F7F"
           />
-          <TouchableOpacity style={styles.buttonBlock} onPress={idCheck}>
-            <ThemedText style={styles.checkText}>확인</ThemedText>
-          </TouchableOpacity>
-        </View>
-        {isDuplicate !== null && (
-          <ThemedText
-            style={[isDuplicate ? styles.duplId : styles.okId, styles.chkMsg]}
-          >
-            {isDuplicate
-              ? "사용 중인 아이디입니다."
-              : "사용 가능한 아이디입니다."}
-          </ThemedText>
-        )}
-        {/* 비밀번호 입력창 */}
-        <TextInput
-          style={[styles.input]}
-          placeholder="비밀번호"
-          secureTextEntry
-          value={password}
-          onChangeText={setPassword}
-          placeholderTextColor="#827F7F"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="비밀번호 확인"
-          secureTextEntry
-          value={passwordChk}
-          onChangeText={setPasswordChk}
-          placeholderTextColor="#827F7F"
-        />
-
-        {/* 이메일 입력창 */}
-        <View style={[styles.twoBlock]}>
           <TextInput
-            style={styles.inputBlock}
-            placeholder="이메일"
-            value={email}
-            onChangeText={setEmail}
+            style={[styles.input, styles.largeMb]}
+            placeholder="비밀번호 확인"
+            secureTextEntry
+            value={passwordChk}
+            onChangeText={setPasswordChk}
             placeholderTextColor="#827F7F"
           />
-          <TouchableOpacity style={styles.buttonBlock} onPress={emailConfirm}>
-            <ThemedText style={styles.checkText}>인증</ThemedText>
-          </TouchableOpacity>
-        </View>
-        {isConfirm !== null && (
-          <ThemedText style={[styles.okId, styles.chkMsg]}>
-            인증 메일이 발송되었습니다.
-          </ThemedText>
-        )}
-        {/* 인증번호 입력창 */}
-        <View style={[styles.twoBlock]}>
+
+          {/* 이메일 입력창 */}
+          <View style={[styles.isText, styles.smallMb]}>
+            <View style={[styles.twoBlock]}>
+              <TextInput
+                style={styles.inputBlock}
+                placeholder="이메일"
+                value={email}
+                onChangeText={setEmail}
+                placeholderTextColor="#827F7F"
+              />
+              <TouchableOpacity
+                style={styles.buttonBlock}
+                onPress={emailConfirm}
+              >
+                <ThemedText style={styles.checkText}>인증</ThemedText>
+              </TouchableOpacity>
+            </View>
+            {isConfirm !== null && (
+              <ThemedText style={styles.okId}>
+                인증 메일이 발송되었습니다.
+              </ThemedText>
+            )}
+          </View>
+          {/* 인증번호 입력창 */}
+          <View style={[styles.isText, styles.largeMb]}>
+            <View style={styles.twoBlock}>
+              <TextInput
+                style={styles.inputBlock}
+                placeholder="인증번호"
+                value={confirmNum}
+                onChangeText={setConfirmNum}
+                placeholderTextColor="#827F7F"
+              />
+              <TouchableOpacity
+                style={styles.buttonBlock}
+                onPress={cofirmNumCheck}
+              >
+                <ThemedText style={styles.checkText}>확인</ThemedText>
+              </TouchableOpacity>
+            </View>
+            {isCorrect !== null && (
+              <ThemedText style={isCorrect ? styles.duplId : styles.okId}>
+                {isCorrect ? "인증번호를 확인해주세요." : "인증되었습니다."}
+              </ThemedText>
+            )}
+          </View>
           <TextInput
-            style={styles.inputBlock}
-            placeholder="인증번호"
-            value={confirmNum}
-            onChangeText={setConfirmNum}
+            style={styles.input}
+            placeholder="닉네임"
+            secureTextEntry
+            value={nickname}
+            onChangeText={setNickName}
             placeholderTextColor="#827F7F"
           />
-          <TouchableOpacity style={styles.buttonBlock} onPress={cofirmNumCheck}>
-            <ThemedText style={styles.checkText}>확인</ThemedText>
-          </TouchableOpacity>
-        </View>
-
-        <TextInput
-          style={styles.input}
-          placeholder="닉네임"
-          secureTextEntry
-          value={nickname}
-          onChangeText={setNickName}
-          placeholderTextColor="#827F7F"
-        />
+        </ThemedView>
         {/*회원가입 버튼*/}
         <TouchableOpacity style={styles.joinButton} onPress={join}>
           <ThemedText style={styles.joinButtonText}>회원가입</ThemedText>
